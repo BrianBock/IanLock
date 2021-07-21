130038770a5c1c3b94b1fe04ef687fcb8fa4ee89
Brian Bock <bbock@terpmail.umd.edu>
2021-07-21 11:00:50 -0400

    Broke out cmd function into lock and unlock functions. Added runtime error for keypad and LCD issues

--- Software/class_main.py
+++ Software/class_main.py
@@ -22,7 +22,7 @@ class ApplePi:
         self.LCD = qwiic_serlcd.QwiicSerlcd()  # set up the LCD
         if not self.LCD.connected:
             print("The Qwiic SerLCD device isn't connected to the system. Please check your connection")
-            sys.exit("The LCD is not connected")
+            raise RuntimeError("The LCD is not connected")
         self.LCD.clearScreen()
         self.LCD.setFastBacklight(255, 255, 255)  # turn the back light on full white
 
@@ -30,7 +30,7 @@ class ApplePi:
         self.keypad = qwiic_keypad.QwiicKeypad()
         if not self.keypad.connected:
             print("The Qwiic Keypad isn't connected to the system. Please check your connection")
-            sys.exit("The Keypad is not connected")
+            raise RuntimeError("The Keypad is not connected")
         self.keypad.begin()
 
         self.lock_status = "UNLOCKED"
@@ -52,28 +52,43 @@ class ApplePi:
         msg = "Door: +" + self.door_status + "\nLock: +" + self.lock_status
         self.LCDclearprint(msg)
 
-    def change_door(self, cmd):
-        if self.door_status is "OPEN" and cmd is "LOCK":
+    # def change_door(self, cmd):
+    #     if self.door_status is "OPEN" and cmd is "LOCK":
+    #         print("Door is open. You cannot lock an open door. Please close the door and try again.")
+    #         self.lock_status = "UNLOCKED"
+    #         return
+    #
+    #     if cmd is "LOCK":
+    #         print("Locking the door...")
+    #         self.motor.rotate(self.motor.cw, 360)
+    #         print("Door is locked")
+    #         self.lock_status = "LOCKED"
+    #
+    #     elif cmd is "UNLOCK":
+    #         print("Unlocking the door...")
+    #         self.motor.rotate(self.motor.ccw, 360)
+    #         print("Door is unlocked")
+    #         self.lock_status = "UNLOCKED"
+    #
+    #     print("Door is " + self.lock_status)
+    #     self.LCDclearprint("Door: " + self.lock_status)
+    def lock(self):
+        """Lock the door"""
+        if self.door_status is "OPEN":
             print("Door is open. You cannot lock an open door. Please close the door and try again.")
             self.lock_status = "UNLOCKED"
             return
+        self.motor.rotate(self.motor.ccw, 360)
+        print("Door is locked")
+        self.lock_status = "LOCKED"
+        self.update_screen_status()
 
-        if cmd is "LOCK":
-            print("Locking the door...")
-            self.motor.rotate(self.motor.cw, 360)
-            print("Door is locked")
-            self.lock_status = "LOCKED"
-
-        elif cmd is "UNLOCK":
-            print("Unlocking the door...")
-            self.motor.rotate(self.motor.ccw, 360)
-            print("Door is unlocked")
-            self.lock_status = "UNLOCKED"
-
-        print("Door is " + self.lock_status)
-        self.LCDclearprint("Door: " + self.lock_status)
-
-
+    def unlock(self):
+        """Unlock the door"""
+        self.motor.rotate(self.motor.cw, 360)
+        print("Door is unlocked")
+        self.lock_status = "UNLOCKED"
+        self.update_screen_status()
 
 # myLCD.display() #turn on display
 # time.sleep(1)
